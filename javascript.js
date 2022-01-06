0d7c6e675f86886bd848fcc4d0d2c0d644c2e425
Lluvib <63505197+Lluvib@users.noreply.github.com>
2022-01-06 21:55:50 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,9 +1,10 @@
-
+// assign values to the identifers.
 const button = document.querySelector('.container button');
 const jokeText =document.querySelector('.container joke p');
-
+// when click getJoke function happens.
 button.addEventListener('click', getJoke);
-
+//async ensures that the function returns a promise
+//await works only inside async function. await waits until that promise settles and returns its result.
 async function getJoke(){
     const jokeData=await fetch('https://icanhazdadjoke.com/',{
         headers:{
@@ -14,3 +15,4 @@ async function getJoke(){
     document.getElementById('joke').innerText = jokeObj.joke;
     console.log(jokeObj);
 };
+//wait until the jokes being fetched successfully, then assign jokes into jason, print out the jokes. 
